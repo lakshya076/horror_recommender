b9350bb55a54006abf3ca18d6bd7774c7d4aff3d
Lakshya <lakshya.saxena076@gmail.com>
2025-07-14 22:56:32 +0530

    misc

--- lib/pages/MovieDetails.dart
+++ lib/pages/MovieDetails.dart
@@ -9,29 +9,31 @@ class MovieDetailPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(movie['title'] ?? 'Movie Detail')),
-      body: SingleChildScrollView(
-        padding: EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            if (movie['poster_path'] != null)
-              Center(
-                child: Image.network(
-                  'https://image.tmdb.org/t/p/w500${movie['poster_path']}',
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              if (movie['poster_path'] != null)
+                Center(
+                  child: Image.network(
+                    'https://image.tmdb.org/t/p/w500${movie['poster_path']}',
+                  ),
                 ),
+              SizedBox(height: 16),
+              Text(
+                movie['title'] ?? '',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
               ),
-            SizedBox(height: 16),
-            Text(
-              movie['title'] ?? '',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 5),
-            Text(movie['tagline']),
-            SizedBox(height: 8),
-            Text("Release Date: ${movie['release_date'] ?? 'N/A'}"),
-            SizedBox(height: 12),
-            Text(movie['overview'] ?? 'No description available.'),
-          ],
+              SizedBox(height: 5),
+              Text(movie['tagline']),
+              SizedBox(height: 8),
+              Text("Release Date: ${movie['release_date'] ?? 'N/A'}"),
+              SizedBox(height: 12),
+              Text(movie['overview'] ?? 'No description available.'),
+            ],
+          ),
         ),
       ),
     );
